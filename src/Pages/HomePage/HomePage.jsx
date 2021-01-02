354a5186b1015cfb24c9ba4810537d8b9edd3ab7
Logan Gunn <lgunn4@uwo.ca>
2021-01-02 18:14:39 -0500

    fixed last commit

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Button, Col, Image, Jumbotron, Row} from "react-bootstrap";
+import {Button, Col, Row} from "react-bootstrap";
 import "./HomePage.css";
 import {Link} from "react-router-dom";
 import NominationCards from "../../Containers/NominationCards/NominationCards";
