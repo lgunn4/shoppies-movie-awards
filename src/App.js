2944eb94021da9c4d5dd3b231c16d9977a5b3dd6
Logan Gunn <lgunn4@uwo.ca>
2021-01-09 18:17:57 -0500

    Refactored App and Layout

--- src/App.js
+++ src/App.js
@@ -1,32 +1,26 @@
-import React, {useEffect} from "react";
+import React from "react";
 import './App.css';
 import 'antd/dist/antd.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Layout from "./Components/Layout/Layout";
 import SearchPage from "./Pages/SearchPage/SearchPage";
-import LoadableComponent from "./Components/LoadableComponent/LoadableComponent";
 import ErrorPage from "./Pages/NotFoundPage/ErrorPage";
 import {BrowserRouter, Switch, Route} from "react-router-dom";
 import HomePage from "./Containers/HomePage/HomePage";
+import Layout from "./Containers/Layout/Layout";
 
 
-function App({fetchNominations, isLoading}) {
-    useEffect(() => {
-        fetchNominations();
-    });
-
+function App() {
     return (
-        <LoadableComponent isLoading={isLoading}>
-            <BrowserRouter>
-                <Layout>
-                    <Switch>
-                        <Route exact path="/search" component = {SearchPage} />
-                        <Route exact path="/" component = {HomePage} />
-                        <Route component={ErrorPage} />
-                    </Switch>
-                </Layout>
-            </BrowserRouter>
-        </LoadableComponent>
+        <BrowserRouter>
+            <Layout>
+                <Switch>
+                    <Route exact path="/search" component = {SearchPage} />
+                    <Route exact path="/" component = {HomePage} />
+                    <Route component={ErrorPage} />
+                </Switch>
+            </Layout>
+        </BrowserRouter>
+
   );
 }
 
